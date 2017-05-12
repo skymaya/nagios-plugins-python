e29404ca629c4c433db627599308e41b04f51132
cyborg <skymaya@gmail.com>
2017-05-12 18:48:02 -0400

    pep8 cleanups

--- check_tcp_port.py
+++ check_tcp_port.py
@@ -1,42 +1,46 @@
 #!/usr/bin/python
 
+"""Nagios plugin to check a tcp connection for a hostname and port"""
+
 # Author: Sky Maya
 # https://github.com/skymaya
+
+from __future__ import print_function
 
 #  standard library imports
 import socket
 import argparse
 import sys
 
-parser = argparse.ArgumentParser(usage='check_tcp_port.py -H host -p port -t timeout')
-parser.add_argument('-H', '--host',
+PARSER = argparse.ArgumentParser(usage='check_tcp_port.py -H host -p port -t timeout')
+PARSER.add_argument('-H', '--host',
                     help='Host to check, i.e. 127.0.0.1',
                     required=True)
-parser.add_argument('-p', '--port',
+PARSER.add_argument('-p', '--port',
                     help='port to check, i.e. 80',
                     required=True)
-parser.add_argument('-t', '--timeout',
+PARSER.add_argument('-t', '--timeout',
                     help='optional timeout, default is 5 seconds',
                     required=False)
-args = parser.parse_args()
+ARGS = PARSER.parse_args()
 
-host = args.host
-port = int(args.port)
+HOST = ARGS.host
+PORT = int(ARGS.port)
 
-if args.timeout:
-    timeout = args.timeout
+if ARGS.timeout:
+    TIMEOUT = ARGS.timeout
 else:
-    timeout = 5
+    TIMEOUT = 5
 
 try:
-    s = socket.socket()
-    s.settimeout(timeout)
-    s.connect((host, port))
-except socket.timeout as e:
-    print('CRITICAL: Connection to port {0} failed: {1}'.format(port, e))
+    SOCK = socket.socket()
+    SOCK.settimeout(TIMEOUT)
+    SOCK.connect((HOST, PORT))
+except socket.timeout as err:
+    print('CRITICAL: Connection to port {0} failed: {1}'.format(PORT, err))
     sys.exit(2)
 else:
-    print('OK: Connection to port {0} successful'.format(port))
+    print('OK: Connection to port {0} successful'.format(PORT))
     sys.exit(0)
 finally:
-    s.close()
+    SOCK.close()
