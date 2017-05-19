93f1c824c5c6a95831454fd0c9687b2448728286
cyborg <skymaya@gmail.com>
2017-05-19 03:16:16 -0400

    replaced netsnmp with pysnmp

--- check_load.py
+++ check_load.py
@@ -8,7 +8,7 @@
 # a host. Accepts warning and critical values as comma-separated floats or integers
 # and compares them to current load levels.
 #
-# REQUIRES: netsnmp (apt-get install libsnmp-python, CentOS just install net-snmp stuff)
+# REQUIRES: pysnmp
 #
 # Example Nagios command for commands.cfg (or where your command templates are stored):
 #
@@ -32,7 +32,7 @@ import argparse
 import sys
 
 # related third party imports
-import netsnmp
+from pysnmp.entity.rfc3413.oneliner import cmdgen
 
 
 class SNMPData(object): # pylint: disable=too-few-public-methods
@@ -42,36 +42,39 @@ class SNMPData(object): # pylint: disable=too-few-public-methods
         self.host = host
 
     @staticmethod
-    def do_snmpget(oid, community, host):
-        """Given an oid, community password, and host, return the result of an
-        snmpget"""
-        var = netsnmp.Varbind(oid)
-        res = netsnmp.snmpget(var, Version=2, DestHost=host, Community=community)
-        return res
+    def do_snmpget(community, host, oid):
+        """Given an list of one or more oids, community password, and host,
+        return the result of an snmpget"""
+        cmd_gen = cmdgen.CommandGenerator()
+        err_found, err_status, err_index, var_binds = cmd_gen.getCmd(
+            cmdgen.CommunityData(community),
+            cmdgen.UdpTransportTarget((host, 161)), *oid)
+        if err_found:
+            print('UNKNOWN: {0} {1} {2}'.format(err_found, err_status, err_index))
+            sys.exit(3)
+        return var_binds
 
 
 class LoadData(SNMPData):
     """Functions to make an SNMP connection to retrieve system load data"""
     def __init__(self, community, host):
-        self.m1_oid = '.1.3.6.1.4.1.2021.10.1.3.1'
-        self.m5_oid = '.1.3.6.1.4.1.2021.10.1.3.2'
-        self.m15_oid = '.1.3.6.1.4.1.2021.10.1.3.3'
+        self.oids = ['1.3.6.1.4.1.2021.10.1.3.1',
+                     '1.3.6.1.4.1.2021.10.1.3.2',
+                     '1.3.6.1.4.1.2021.10.1.3.3']
+        self.data = self.do_snmpget(community, host, self.oids)
         super(LoadData, self).__init__(community, host)
 
     def one_minute(self):
         """Return the one minute load average"""
-        load = self.do_snmpget(self.m1_oid, self.community, self.host)
-        return load[0]
+        return self.data[0][1]
 
     def five_minute(self):
         """Return the five minute load average"""
-        load = self.do_snmpget(self.m5_oid, self.community, self.host)
-        return load[0]
+        return self.data[1][1]
 
     def fifteen_minute(self):
         """Return the fifteen minute load average"""
-        load = self.do_snmpget(self.m15_oid, self.community, self.host)
-        return load[0]
+        return self.data[2][1]
 
 
 def do_argparser():
